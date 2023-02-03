4987f2b5337240dc4d3ee697ed11b82312ca25b2
omodaraodubiro <omodaraodubiro@gmail.com>
2023-02-03 13:34:48 +0100

    added the search engine

--- src/vanilla.js
+++ src/vanilla.js
@@ -41,8 +41,19 @@ function formatDate(timestamp) {
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
   }
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let city = "nigeria";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  function search(city){
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(displayTemperature);
+    
+  }
+  function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+  }
   
+  search("new york")
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
