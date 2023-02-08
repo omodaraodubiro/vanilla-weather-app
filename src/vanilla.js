f3beca20e0d952799b4f4fd880776e211bbbb560
omodaraodubiro <omodaraodubiro@gmail.com>
2023-02-08 09:54:00 +0100

    added a function to display the weather forecast,not getting live API yet

--- src/vanilla.js
+++ src/vanilla.js
@@ -21,6 +21,37 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
+
+  function displayForecast() {
+  let forecastElement = document.querySelector("#forcast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src='http://openweathermap.org/img/wn/50d@2x.png'
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
   function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -29,6 +60,7 @@ function formatDate(timestamp) {
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
+
     celsiusTemperature = response.data.main.temp;
 
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
@@ -83,3 +115,4 @@ function formatDate(timestamp) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
   search("new york");
+  displayForecast();
