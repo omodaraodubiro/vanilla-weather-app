8001ec3a068de36ea302089e20077faaec5b6c60
omodaraodubiro <omodaraodubiro@gmail.com>
2023-02-09 09:20:25 +0100

    made the app responsive and i added an error catch to alert a message when users type a city that does not exist and clear the search bar for you to type a new city

--- src/vanilla.js
+++ src/vanilla.js
@@ -95,7 +95,16 @@ function getForecast(coordinates) {
   function search(city){
     let apiKey = "5305adb02467a5ede57c1ffc97012175";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-   axios.get(apiUrl).then(displayTemperature);
+    axios
+    .get(apiUrl)
+    .then(displayTemperature)
+    .catch(function (error) {
+      if (error.response.status === 404) {
+        let cityInputElement = document.querySelector("#city-input");
+        cityInputElement.value = "";
+        alert("City does not exist!😟");
+      }
+    });
     
   }
   function handleSubmit(event){
